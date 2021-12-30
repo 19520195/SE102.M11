e5d2d4571d851aed7961df4b5e792ad6a98395ca
nhalq <nhalq287@gmail.com>
2021-12-30 08:05:40 +0700

    GX680 Animation

--- BlasterMaster/Source/Object/Enemy/GX680.cc
+++ BlasterMaster/Source/Object/Enemy/GX680.cc
@@ -1,11 +1,65 @@
 #include "GX680.hh"
+#include "Engine/Core/Game.hh"
+#include "Scene/PlayScene.hh"
+
+constexpr float GX680_WALK_SPEED = 0.04f;
 
 GX680::GX680()
-{ 
-  m_Render = CreateRef<Sprite>(0.f, 0.f, 16.f, 16.f, TextureBase::GetInstance()->Get("RBB"));
+{
+  SetWalkState(GX680WalkState::Left);
+}
+
+void GX680::SetWalkState(GX680WalkState ws)
+{
+  if (m_WalkState != ws)
+  {
+    switch (m_WalkState = ws)
+    {
+    case GX680WalkState::Left:
+      SetAnimation("GX680-Walk-Left");
+      break;
+
+    case GX680WalkState::Right:
+      SetAnimation("GX680-Walk-Right");
+      break;
+    }
+  }
+}
+
+void GX680::Update()
+{
+  auto scene = std::static_pointer_cast<PlayScene>(
+    Game::GetInstance()->GetScene());
+  auto player = scene->GetPlayer();
+  
+  Vector2F velocity = player->GetLocation() - this->GetLocation();
+  velocity = velocity / velocity.Abs();
+  velocity = velocity * GX680_WALK_SPEED;
+  m_Velocity = std::move(velocity);
+
+  if (m_Velocity.GetX() < 0)
+    SetWalkState(GX680WalkState::Left);
+  else SetWalkState(GX680WalkState::Right);
 }
 
 GX680S::GX680S()
 {
-  m_Render = CreateRef<Sprite>(0.f, 0.f, 16.f, 16.f, TextureBase::GetInstance()->Get("RBB"));
+  SetAnimation("GX680S-Walk-Left");
+}
+
+void GX680S::SetWalkState(GX680WalkState ws)
+{
+  if (m_WalkState != ws)
+  {
+    switch (m_WalkState = ws)
+    {
+    case GX680WalkState::Left:
+      SetAnimation("GX680S-Walk-Left");
+      break;
+
+    case GX680WalkState::Right:
+      SetAnimation("GX680S-Walk-Right");
+      break;
+    }
+  }
 }
--- BlasterMaster/Source/Object/Enemy/GX680.hh
+++ BlasterMaster/Source/Object/Enemy/GX680.hh
@@ -1,14 +1,26 @@
 #pragma once
 #include "Enemy.hh"
 
+enum class GX680WalkState : uint8_t
+{
+  Left,
+  Right,
+};
+
 class GX680 : public Enemy
 {
 public:
   GX680();
+  virtual void SetWalkState(GX680WalkState ws);
+  void Update() override;
+
+protected:
+  GX680WalkState m_WalkState;
 };
 
 class GX680S : public GX680
 {
 public:
   GX680S();
+  virtual void SetWalkState(GX680WalkState ws);
 };
